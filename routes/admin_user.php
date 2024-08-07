c6e2cb9475b8b060f4558967e3fd5aa6ca2628cc
cperezsola <cperezsola@gmail.com>
2024-08-07 20:56:52 +0200

    Continue creating variant products

--- routes/admin_user.php
+++ routes/admin_user.php
@@ -60,3 +60,9 @@ Route::resource('products-variant', ProductVariantController::class);
 
 /** Products variant item route */
 Route::get('products-variant-item/{productId}/{variantId}', [ProductVariantItemController::class, 'index'])->name('products-variant-item.index');
+Route::get('products-variant-item/create/{productId}/{variantId}', [ProductVariantItemController::class, 'create'])->name('products-variant-item.create');
+Route::post('products-variant-item', [ProductVariantItemController::class, 'store'])->name('products-variant-item.store');
+Route::get('products-variant-item-edit/{variantItemId}', [ProductVariantItemController::class, 'edit'])->name('products-variant-item.edit');
+
+
+Route::delete('products-variant-item/{variantItemId}', [ProductVariantItemController::class, 'destroy'])->name('products-variant-item.destroy');
