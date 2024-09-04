f7c869cdbf5b490f92b3a3ff2fa8412f15ff6580
cperezsola <cperezsola@gmail.com>
2024-09-04 19:46:08 +0200

    First part of Paypal Integration and start creating sandbox

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Models\PaypalSetting;
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 class PaymentController extends Controller
 {
@@ -14,5 +16,104 @@ class PaymentController extends Controller
             return redirect()->route('user.checkout');
         }
         return view('frontend.pages.payment');
+    }
+
+    public function paypalConfig()
+    {
+        $paypalSetting = PaypalSetting::first();
+        $config = [
+            'mode'    => $paypalSetting->mode === 1 ? 'live' : 'sandbox',
+            'sandbox' => [
+                'client_id'         => $paypalSetting->client_id,
+                'client_secret'     => $paypalSetting->secret_key,
+                'app_id'            => 'APP-80W284485P519543T',
+            ],
+            'live' => [
+                'client_id'         => $paypalSetting->client_id,
+                'client_secret'     => $paypalSetting->secret_key,
+                'app_id'            => '',
+            ],
+
+            'payment_action' => 'Sale',
+            'currency'       => $paypalSetting->currency_name,
+            'notify_url'     => '',
+            'locale'         => 'en_US',
+            'validate_ssl'   =>  true,
+        ];
+        return $config;
+    }
+
+    /** Paypal redirect */
+    public function payWithPaypal()
+    {
+        $config = $this->paypalConfig();
+        $paypalSetting = PaypalSetting::first();
+
+        $provider = new PayPalClient($config);
+        $provider->getAccessToken();
+
+
+        // calculate payable amount depending on currency rate
+        $total = getFinalPayableAmount();
+        $payableAmount = round($total * $paypalSetting->currency_rate, 2);
+
+
+        $response = $provider->createOrder([
+            "intent" => "CAPTURE",
+            "application_context" => [
+                "return_url" => route('user.paypal.success'),
+                "cancel_url" => route('user.paypal.cancel'),
+            ],
+            "purchase_units" => [
+                [
+                    "amount" => [
+                        "currency_code" => $config['currency'],
+                        "value" => $payableAmount
+                    ]
+                ]
+            ]
+        ]);
+
+        if (isset($response['id']) && $response['id'] != null) {
+            foreach ($response['links'] as $link) {
+                if ($link['rel'] === 'approve') {
+                    return redirect()->away($link['href']);
+                }
+            }
+        } else {
+            return redirect()->route('user.paypal.cancel');
+        }
+    }
+    
+    public function paypalSuccess(Request $request)
+    {
+        $config = $this->paypalConfig();
+        $provider = new PayPalClient($config);
+        $provider->getAccessToken();
+
+        $response = $provider->capturePaymentOrder($request->token);
+
+        if (isset($response['status']) && $response['status'] == 'COMPLETED') {
+
+            // calculate payable amount depending on currency rate
+            $paypalSetting = PaypalSetting::first();
+            $total = getFinalPayableAmount();
+            $paidAmount = round($total*$paypalSetting->currency_rate, 2);
+
+            $this->storeOrder('paypal', 1, $response['id'], $paidAmount, $paypalSetting->currency_name);
+
+            // clear session
+            $this->clearSession();
+
+            return redirect()->route('user.payment.success');
+        }
+
+        return redirect()->route('user.paypal.cancel');
+    }
+
+    public function paypalCancel()
+    {
+        toastr('Someting went wrong try agin later!', 'error', 'Error');
+        return redirect()->route('user.payment');
     }
 }
