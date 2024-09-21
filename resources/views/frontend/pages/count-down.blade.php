59b254599590b8a78319db980264d9e390e5a3ae
cperezsola <cperezsola@gmail.com>
2024-09-21 17:27:08 +0200

    Continue reducing the amont of querys and cache some sections for better time charges

--- resources/views/frontend/pages/count-down.blade.php
+++ resources/views/frontend/pages/count-down.blade.php
@@ -34,196 +34,22 @@
                 </div>
             </div>
             <div class="row">
-                @foreach ($countDownItems as $item)
-                    @php
-                        $product = \App\Models\Product::find($item->product_id);
-                    @endphp
-                    <div class="col-xl-3 col-sm-6 col-lg-4">
-                        <div class="wsus__product_item">
-                            <span class="wsus__new">{{ productType($product->product_type) }}</span>
-                            @if (checkDiscount($product))
-                                <span
-                                    class="wsus__minus">-{{ calculateDiscountPercent($product->price, $product->offer_price) }}%</span>
-                            @endif
-                            <a class="wsus__pro_link" href="{{ route('product-detail', $product->slug) }}">
-                                <img src="
-                                        @if (isset($product->productImageGalleries[0]->image)) {{ asset($product->productImageGalleries[0]->image) }}
-                                        @else
-                                            {{ asset($product->thumb_image) }} @endif
-                                    "
-                                    alt="product" class="img-fluid w-100 img_2" />
-                            </a>
-                            <ul class="wsus__single_pro_icon">
-                                <li><a href="#" data-bs-toggle="modal"
-                                        data-bs-target="#exampleModal-{{ $product->id }}"><i class="far fa-eye"></i></a>
-                                </li>
-                                <li><a href="#"><i class="far fa-heart"></i></a></li>
-                                <li><a href="#"><i class="far fa-random"></i></a>
-                            </ul>
-                            <div class="wsus__product_details">
-                                <a class="wsus__category" href="#">{{ $product->category->name }} </a>
-                                <p class="wsus__pro_rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star-half-alt"></i>
-                                    <span>(133 review)</span>
-                                </p>
-                                <a class="wsus__pro_name"
-                                    href="{{ route('product-detail', $product->slug) }}">{{ $product->name }}</a>
-                                @if (checkDiscount($product))
-                                    <p class="wsus__price">{{ $settings->currency_icon }}{{ $product->offer_price }}
-                                        <del>{{ $product->price }}</del>
-                                    </p>
-                                @else
-                                    <p class="wsus__price">{{ $settings->currency_icon }}{{ $product->price }}</p>
-                                @endif
-                                <form class="shopping-cart-form">
-                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
-                                    @foreach ($product->variants as $variant)
-                                        <select class="d-none" name="variants_items[]">
-                                            @foreach ($variant->productVariantItems as $variantItem)
-                                                <option value="{{ $variantItem->id }}"
-                                                    {{ $variantItem->is_default == 1 ? 'selected' : '' }}>
-                                                    {{ $variantItem->name }}
-                                                    ({{ $variantItem->price }}€)
-                                                </option>
-                                            @endforeach
-                                        </select>
-                                    @endforeach
-                                    <input name="qty" type="hidden" min="1" max="100" value="1" />
-                                    <button class="add_cart" href="#" type="submit">add to cart</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                @php
+                    $products = \App\Models\Product::with(['variants', 'category', 'productImageGalleries'])
+                        ->whereIn('id', $countDownItems)
+                        ->get();
+                @endphp
+                @foreach ($products as $product)
+                    <x-product-card :product="$product" />
                 @endforeach
             </div>
-            <div class="mt-5">
+            {{-- <div class="mt-5">
                 @if ($countDownItems->hasPages())
                     {{ $countDownItems->links() }}
                 @endif
-            </div>
+            </div> --}}
         </div>
     </section>
-
-    @foreach ($countDownItems as $item)
-        @php
-            $product = \App\Models\Product::find($item->product_id);
-        @endphp
-        <section class="product_popup_modal">
-            <div class="modal fade" id="exampleModal-{{ $product->id }}" tabindex="-1" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i
-                                    class="far fa-times"></i></button>
-                            <div class="row">
-                                <div class="col-xl-6 col-12 col-sm-10 col-md-8 col-lg-6 m-auto display">
-                                    <div class="wsus__quick_view_img">
-                                        @if ($product->video_link)
-                                            <a class="venobox wsus__pro_det_video" data-autoplay="true" data-vbtype="video"
-                                                href="{{ $product->video_link }}">
-                                                <i class="fas fa-play"></i>
-                                            </a>
-                                        @endif
-                                        <div class="row modal_slider">
-                                            <div class="col-xl-12">
-                                                <div class="modal_slider_img">
-                                                    <img src="{{ asset($product->thumb_image) }}"
-                                                        alt="{{ asset($product->name) }}" class="img-fluid w-100">
-                                                </div>
-                                            </div>
-                                            @if (count($product->productImageGalleries) == 0)
-                                                <div class="col-xl-12">
-                                                    <div class="modal_slider_img">
-                                                        <img src="{{ asset($product->thumb_image) }}"
-                                                            alt="{{ asset($product->name) }}" class="img-fluid w-100">
-                                                    </div>
-                                                </div>
-                                            @endif
-                                            @foreach ($product->productImageGalleries as $productImage)
-                                                <div class="col-xl-12">
-                                                    <div class="modal_slider_img">
-                                                        <img src="{{ asset($productImage->image) }}"
-                                                            alt="{{ asset($product->name) }}" class="img-fluid w-100">
-                                                    </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6 col-12 col-sm-12 col-md-12 col-lg-6">
-                                    <div class="wsus__pro_details_text">
-                                        <a class="title" href="#">{{ $product->name }}</a>
-                                        @if ($product->qty > 0)
-                                            <p class="wsus__stock_area"><span class="in_stock">in stock</span>
-                                                ({{ $product->qty }}
-                                                item)
-                                            </p>
-                                        @elseif ($product->qty === 0)
-                                            <p class="wsus__stock_area"><span class="in_stock">stock out</span>
-                                                ({{ $product->qty }}
-                                                item)</p>
-                                        @endif
-                                        @if (checkDiscount($product))
-                                            <h4>{{ $settings->currency_icon }}{{ $product->currency_icon }}{{ $product->offer_price }}
-                                                <del>{{ $settings->currency_icon }}{{ $product->currency_icon }}{{ $product->price }}</del>
-                                            </h4>
-                                        @else
-                                            <h4>{{ $settings->currency_icon }}{{ $product->currency_icon }}{{ $product->price }}
-                                            </h4>
-                                        @endif
-                                        <p class="description">{!! $product->short_description !!}</p>
-                                        <form class="shopping-cart-form">
-                                            <div class="wsus__selectbox">
-                                                <div class="row">
-                                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
-                                                    @foreach ($product->variants as $variant)
-                                                        @if ($variant->status != 0)
-                                                            <div class="col-xl-6 col-sm-6">
-                                                                <h5 class="mb-2">{{ $variant->name }}: </h5>
-                                                                <select class="select_2" name="variants_items[]">
-                                                                    @foreach ($variant->productVariantItems as $variantItem)
-                                                                        @if ($variantItem->status != 0)
-                                                                            <option value="{{ $variantItem->id }}"
-                                                                                {{ $variantItem->is_default == 1 ? 'selected' : '' }}>
-                                                                                {{ $variantItem->name }}
-                                                                                ({{ $variantItem->price }}€)
-                                                                            </option>
-                                                                        @endif
-                                                                    @endforeach
-                                                                </select>
-                                                            </div>
-                                                        @endif
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                            <div class="wsus__quentity">
-                                                <h5>Quantity:</h5>
-                                                <div class="select_number">
-                                                    <input class="number_area" name="qty" type="text"
-                                                        min="1" max="100" value="1" />
-                                                </div>
-                                            </div>
-                                            <ul class="wsus__button_area">
-                                                <li>
-                                                    <button type="submit" class="add_cart" href="#">add to
-                                                        cart</button>
-                                                </li>
-                                            </ul>
-                                        </form>
-                                        <p class="brand_model"><span>brand :</span> {{ $product->brand->name }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    @endforeach
 @endsection
 
 @push('scripts')
