7214613c4d35ab628d52915f169f0f3dc02a8079
cperezsola <cperezsola@gmail.com>
2024-09-22 19:32:17 +0200

    Second part reparing minor bug and protecting relations in variants

--- resources/views/admin_user/product/product-variant-item/index.blade.php
+++ resources/views/admin_user/product/product-variant-item/index.blade.php
@@ -1,62 +1,54 @@
 @extends('admin_user.layouts.main')
 
 @section('container')
-      <!-- Main Content -->
-        <section class="section">
-          <div class="section-header">
+    <section class="section">
+        <div class="section-header">
             <h1>Product Variant Items</h1>
-          </div>
-          <div class="mb-3">
-            <a href="{{route('admin_user.products-variant.index', ['product' => $product->id])}}" class="btn btn-primary">Back</a>
-          </div>
-          <div class="section-body">
-
+        </div>
+        <div class="mb-3">
+            <a href="{{ route('admin_user.products-variant.index', ['product' => $product->id]) }}" class="btn btn-primary">Back</a>
+        </div>
+        <div class="section-body">
             <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <div class="card-header">
-                    <h4>Variant: {{$variant->name}} </h4>
-                    <div class="card-header-action">
-                        <a href="{{route('admin_user.products-variant-item.create', ['productId' => $product->id, 'variantId' => $variant->id])}}" class="btn btn-primary"><i class="fas fa-plus"></i> Create New</a>
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Variant: {{ $variant->name }} </h4>
+                            <div class="card-header-action">
+                                <a href="{{ route('admin_user.products-variant-item.create', ['productId' => $product->id, 'variantId' => $variant->id]) }}" class="btn btn-primary"><i class="fas fa-plus"></i> Create New</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            {{ $dataTable->table() }}
+                        </div>
                     </div>
-                  </div>
-                  <div class="card-body">
-                    {{ $dataTable->table() }}
-                  </div>
-
                 </div>
-              </div>
             </div>
-
-          </div>
-        </section>
-
+        </div>
+    </section>
 @endsection
 
 @push('scripts')
     {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
-
     <script>
-        $(document).ready(function(){
-            $('body').on('click', '.change-status', function(){
+        $(document).ready(function() {
+            $('body').on('click', '.change-status', function() {
                 let isChecked = $(this).is(':checked');
                 let id = $(this).data('id');
-
                 $.ajax({
-                    url: "{{route('admin_user.products-variant-item.changes-status')}}",
+                    url: "{{ route('admin_user.products-variant-item.changes-status') }}",
                     method: 'PUT',
                     data: {
                         status: isChecked,
                         id: id
                     },
-                    success: function(data){
+                    success: function(data) {
                         toastr.success(data.message)
                     },
-                    error: function(xhr, status, error){
+                    error: function(xhr, status, error) {
                         console.log(error);
                     }
                 })
-
             })
         })
     </script>
