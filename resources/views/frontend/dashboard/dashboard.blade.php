19e67a7efab506d0642198c140f6382b3129c3dc
cperezsola <cperezsola@gmail.com>
2024-09-24 20:27:35 +0200

    Copy same features from user to third party user

--- resources/views/frontend/dashboard/dashboard.blade.php
+++ resources/views/frontend/dashboard/dashboard.blade.php
@@ -18,28 +18,28 @@
                                 <div class="col-xl-3 col-6 col-md-4">
                                     <a class="wsus__dashboard_item red" href="{{ route('user.orders.index') }}">
                                         <i class="fas fa-cart-plus"></i>
-                                        <p>Total Order</p>
+                                        <p class="lh-1">Total Order</p>
                                         <h4 style="color:#ffff">{{ $totalOrder }}</h4>
                                     </a>
                                 </div>
                                 <div class="col-xl-3 col-6 col-md-4">
                                     <a class="wsus__dashboard_item green" href="dsahboard_download.html">
                                         <i class="fas fa-cart-plus"></i>
-                                        <p>Pending Orders</p>
+                                        <p class="lh-1">Pending Orders</p>
                                         <h4 style="color:#ffff">{{ $pendingOrder }}</h4>
                                     </a>
                                 </div>
                                 <div class="col-xl-3 col-6 col-md-4">
                                     <a class="wsus__dashboard_item sky" href="dsahboard_review.html">
                                         <i class="fas fa-cart-plus"></i>
-                                        <p>Complete Orders</p>
+                                        <p class="lh-1">Complete Orders</p>
                                         <h4 style="color:#ffff">{{ $completeOrder }}</h4>
                                     </a>
                                 </div>
                                 <div class="col-xl-3 col-6 col-md-4">
                                     <a class="wsus__dashboard_item orange" href="{{ route('user.profile') }}">
                                         <i class="fas fa-user-shield"></i>
-                                        <p>profile</p>
+                                        <p class="lh-1">profile</p>
                                         <h4 style="color:#ffff">-</h4>
                                     </a>
                                 </div>
--- resources/views/third_party_user/layouts/sidebar.blade.php
+++ resources/views/third_party_user/layouts/sidebar.blade.php
@@ -6,18 +6,16 @@
     <a href="javascript:;" class="dash_logo"><img src="{{ asset($logoSetting->logo) }}" alt="logo" class="img-fluid"></a>
     <ul class="dashboard_link">
         <li><a class="{{ setActive(['third_party_user.dashboard']) }}" href="{{ route('third_party_user.dashboard') }}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
-            <li><a class="" href="{{ route('home') }}"><i class="fas fa-home"></i>Go To Home</a></li>
-            <li><a class="{{ setActive(['third_party_user.orders.*']) }}" href="{{ route('third_party_user.orders.index') }}"><i class="fas fa-box"></i> Orders</a></li>
-            <li><a class="{{ setActive(['third_party_user.products.*']) }}" href="{{ route('third_party_user.products.index') }}"><i class="fas fa-cart-plus"></i> Products</a></li>
-            <li><a class="{{ setActive(['third_party_user.shop-profile.index']) }}" href="{{ route('third_party_user.shop-profile.index') }}"><i class="far fa-user"></i> Shop Profile</a></li>
-            <li><a class="{{ setActive(['third_party_user.profile']) }}" href="{{ route('third_party_user.profile') }}"><i class="far fa-user"></i> My Profile</a></li>
+        <li><a class="" href="{{ route('home') }}"><i class="fas fa-home"></i>Go To Home</a></li>
+        <li><a class="{{ setActive(['third_party_user.orders.*']) }}" href="{{ route('third_party_user.orders.index') }}"><i class="fas fa-box"></i> Orders</a></li>
+        <li><a class="{{ setActive(['third_party_user.products.*']) }}" href="{{ route('third_party_user.products.index') }}"><i class="fas fa-cart-plus"></i> Products</a></li>
+        <li><a class="{{ setActive(['third_party_user.shop-profile.index']) }}" href="{{ route('third_party_user.shop-profile.index') }}"><i class="far fa-user"></i> Shop Profile</a></li>
+        <li><a class="{{ setActive(['third_party_user.profile']) }}" href="{{ route('third_party_user.profile') }}"><i class="far fa-user"></i> My Profile</a></li>
         <li>
-
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();"><i class="far fa-sign-out-alt"></i> Log out</a>
             </form>
         </li>
-
     </ul>
 </div>
