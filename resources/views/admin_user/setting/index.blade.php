a5cb8010e8b68b193b0dfe84886e946eac6ad4cf
cperezsola <cperezsola@gmail.com>
2024-09-25 20:18:19 +0200

    Delete useless item from setting menu

--- resources/views/admin_user/setting/index.blade.php
+++ resources/views/admin_user/setting/index.blade.php
@@ -18,9 +18,7 @@
                                         <a class="list-group-item list-group-item-action" id="list-profile-list"
                                             data-toggle="list" href="#list-profile" role="tab">Email Configuration</a>
                                         <a class="list-group-item list-group-item-action" id="list-messages-list"
-                                            data-toggle="list" href="#list-messages" role="tab">Logo and Favicon</a>
-                                        <a class="list-group-item list-group-item-action" id="list-pusher-list"
-                                            data-toggle="list" href="#pusher-setting" role="tab">Pusher Setting</a>
+                                            data-toggle="list" href="#list-messages" role="tab">Logo and Favicon</a>                                        
                                     </div>
                                 </div>
                                 <div class="col-10">
@@ -28,7 +26,6 @@
                                         @include('admin_user.setting.general-setting')
 										@include('admin_user.setting.email-configuration')
                                         @include('admin_user.setting.logo-setting')
-                                        {{-- @include('admin_user.setting.pusher-setting') --}}
                                     </div>
                                 </div>
                             </div>
